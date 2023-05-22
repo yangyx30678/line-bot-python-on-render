02a304fbb1fe135777a1c56dd2055fc9a3997044
yangyx30678 <71892273+yangyx30678@users.noreply.github.com>
2023-05-23 02:59:28 +0800

    add txt files to store food suggestion

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+
 line_bot_api = LineBotApi('TjUdD+lC12qAYmVLoB0yJnu7TWSP6mkbHqRPpsVRid7coqsQ7ikywxfyXn+3ZlBuGMsnf8qCW8aCykv+ekI1xZ9BbaUzaFkQp3GYNzVKwU63Q1zqnqQWWGUjurhVvn3pEEA9pfMRJhAQluhpvusQoQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('f2b089725e9ef1d7dfe05bd04a3c8f47')
 
@@ -26,6 +27,7 @@ def callback():
     #回覆OK
     return 'OK'
 
+import random
 
 @handler.add(MessageEvent, message=TextMessage)
 #加入一個handle_message function
@@ -33,7 +35,15 @@ def handle_message(event):
     # line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text+"((歪頭((燦笑"))
     my_text = event.message.text
     if my_text == "我想吃大餐":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="吃屎吧你"))
+        with open('feast.txt') as f:
+            lines = f.readlines()
+            output = random.choice(lines)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=output))
+    elif my_text == "我想吃小吃":
+        with open('streetfood.txt') as f:
+            lines = f.readlines()
+            output = random.choice(lines)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=output))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text+"((歪頭((燦笑"))
 
