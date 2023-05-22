e7a3180c05a4cbced338b83501e27205ab6b946a
yangyx30678 <71892273+yangyx30678@users.noreply.github.com>
2023-05-23 02:15:07 +0800

    add files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 #import flask
-#XXXXXXX
+from flask import Flask, request, abort
 from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
